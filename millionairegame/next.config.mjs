6aa40c3ba1ef95f559feae2a364537a7c285412e
YauhenYN <72869403+YauhenYN@users.noreply.github.com>
2024-04-03 13:16:15 +0300

    Update next.config.mjs

--- millionairegame/next.config.mjs
+++ millionairegame/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/whowantstobecomeamillionaire",
-    output: "export",
-    reactStrictMode: true,
 };
 
 export default nextConfig;
