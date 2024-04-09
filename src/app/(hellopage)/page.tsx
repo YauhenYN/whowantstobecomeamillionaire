cd9509c0f21f60609328975febeae861c32f7357
YauhenYN <qwertyuiop1709@yandex.ru>
2024-04-09 11:18:02 +0300

    fixed images for deploy

--- src/app/(hellopage)/page.tsx
+++ src/app/(hellopage)/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
       <div className="bg-blue-200 rounded-md overflow-hidden relative h-[750px] w-[400px] flex flex-col items-center justify-center">
         <div className="absolute left-0 top-0 w-full h-full ">
-          <Image src="/galaxy.jpg" alt="Galaxy" fill className="object-center object-cover"></Image>
+          <Image src="/whowantstobecomeamillionaire/galaxy.jpg" alt="Galaxy" fill className="object-center object-cover"></Image>
         </div>
         <div className="z-[1] flex flex-col items-center mt-[-50px]">
-          <Image src="/logo.png" alt="Logo" height={150} width={150}></Image>
+          <Image src="/whowantstobecomeamillionaire/logo.png" alt="Logo" height={150} width={150}></Image>
           <h1 className="text-[18px] text-center max-w-[280px] pt-[18px] leading-[1.2]">Приветствуем, это <br/>Игра<br/><span className="text-pink-200 text-[20px] font-bold">&quot;Как стать миллионером&quot;</span></h1>
           <div className="pt-[30px]">
             <Link href="/play" className="font-bold text-[18px] bg-blue-950 px-[35px] py-[12px] hover:bg-blue-900 rounded-md">Играть</Link>
